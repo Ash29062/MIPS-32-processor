efafdd459b940c228de899360ef43c3c8251939a
Ash29062 <120658535+Ash29062@users.noreply.github.com>
2025-06-14 17:24:36 +0530

    added files

---
+++ Mips 32 sim1/project_1.srcs/sim_1/new/Mips32_tb1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.06.2025 15:25:39
+// Design Name: 
+// Module Name: Mips32_tb1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mips32_tb1(
+
+    );
+endmodule
